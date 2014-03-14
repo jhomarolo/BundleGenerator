2f19aa3888c2ca0497181bc9bd9823376b20c14b
jhonathan <jhonathan.souza@crescabrasil.com.br>
2014-03-14 16:19:33 -0300

    V1

---
+++ BundleGenerator/Upload/FilesStatus.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace BundleGenerator.Upload
+{
+    public class FilesStatus
+    {
+        public const string HandlerPath = "/Upload/";
+
+        public string group { get; set; }
+        public string name { get; set; }
+        public string realname { get; set; }
+        public string type { get; set; }
+        public int size { get; set; }
+        public string progress { get; set; }
+        public string url { get; set; }
+        public string thumbnail_url { get; set; }
+        public string delete_url { get; set; }
+        public string delete_type { get; set; }
+        public string error { get; set; }
+
+        public FilesStatus() { }
+
+        public FilesStatus(FileInfo fileInfo) { SetValues(fileInfo.Name, (int)fileInfo.Length, fileInfo.FullName); }
+
+        public FilesStatus(string fileName, int fileLength, string fullPath) { SetValues(fileName, fileLength, fullPath); }
+
+        private void SetValues(string fileName, int fileLength, string fullPath)
+        {
+            name = fileName;
+            realname = fileName.Substring(fileName.IndexOf("$") + 1);
+            type = "image/png";
+            size = fileLength;
+            progress = "1.0";
+            url = HandlerPath + "UploadHandler.ashx?f=" + fileName;
+            delete_url = HandlerPath + "UploadHandler.ashx?f=" + fileName;
+            delete_type = "DELETE";
+
+            var ext = Path.GetExtension(fullPath);
+            
+            var fileSize = ConvertBytesToMegabytes(new FileInfo(fullPath).Length);
+            if (IsJS(ext)) thumbnail_url = "/Content/img/js.png";
+            else if ( IsCSS(ext)) thumbnail_url = "/Content/img/css.png";
+            else thumbnail_url = "/Content/img/file.png"; // thumbnail_url = @"data:image/png;base64," + EncodeFile(fullPath);
+        }
+
+        private bool IsJS(string ext)
+        {
+            return ext == ".js";
+        }
+        private bool IsCSS(string ext)
+        {
+            return ext == ".css";
+        }
+
+        private string EncodeFile(string fileName)
+        {
+            return Convert.ToBase64String(System.IO.File.ReadAllBytes(fileName));
+        }
+
+        static double ConvertBytesToMegabytes(long bytes)
+        {
+            return (bytes / 1024f) / 1024f;
+        }
+    }
+}
